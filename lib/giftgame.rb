83bec0881d7d95845d1c48afef54875650f69f7c
Albert Bellonch <albert@itnig.net>
2011-12-19 19:54:26 +0100

    Added my fastest version and two tests

--- lib/giftgame.rb
+++ lib/giftgame.rb
@@ -1,3 +1,5 @@
-def count_gifts(gifts, day)
-  55
+module Giftgame
+  def self.count_gifts(gifts, day)
+    gifts.split("\n").map{|n|n=n.to_i;n>0 ? n : 1}.reverse.take(day).inject(:+)
+  end
 end
--- spec/integration/giftgame_spec.rb
+++ spec/integration/giftgame_spec.rb
@@ -1,7 +1,43 @@
 require 'spec_helper'
 
 describe Giftgame do
-  it "should go well" do
-    true.should be_true
+  it "should go well with the web's problem" do
+    gifts = <<-'GIFTS'
+12 Drummers Drumming
+11 Pipers Piping
+10 Lords-a-Leaping
+9 Ladies Dancing
+8 Maids-a-Milking
+7 Swans-a-Swimming
+6 Geese-a-Laying
+5 Gold Rings
+4 Colly Birds
+3 French Hens
+2 Turtle Doves
+A Partridge in a Pear Tree.
+    GIFTS
+    day = 10
+
+    Giftgame.count_gifts(gifts, day).should == 55
+  end
+
+  it "should go well with another problem" do
+    gifts = <<-'GIFTS'
+12 Drummers Drumming
+11 Pipers Piping
+10 Lords-a-Leaping
+A Ladies Dancing
+8 Maids-a-Milking
+7 Swans-a-Swimming
+3 Geese-a-Laying
+5 Gold Rings
+4 Colly Birds
+3 French Hens
+2 Turtle Doves
+A Partridge in a Pear Tree.
+    GIFTS
+    day = 10
+
+    Giftgame.count_gifts(gifts, day).should == 44
   end
 end
